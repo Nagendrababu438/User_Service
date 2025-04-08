1b12e21cc4eb509eb12a2ba32d878df913e87d8f
Admin <nagendhrababu438@gmail.com>
2025-04-09 00:59:41 +0530

    Initial commit

---
+++ src/main/java/dev/naman/userservicetestfinal/clients/KafkaProducerClient.java
@@ -0,0 +1,23 @@
+package dev.naman.userservicetestfinal.clients;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerClient {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerClient(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+
+
+
+}
